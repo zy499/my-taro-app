1c6bebbc9199ee13b32dbc84ef2741b9e3e7f7d4
zy499 <hqh499@gmail.com>
2022-11-02 01:15:54 +0800

    feat: 初始化

---
+++ config/index.js
@@ -0,0 +1,124 @@
+import AutoImport from 'unplugin-auto-import/webpack'
+import Components from 'unplugin-vue-components/webpack'
+const config = {
+  projectName: 'my-taro-app',
+  date: '2022-11-2',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
+    375: 2 / 1
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: ['@tarojs/plugin-html', ['taro-plugin-unocss', {
+    preset: {
+      remToRpx: {
+        baseFontSize: 4,
+      },
+    },
+  }]],
+  defineConstants: {
+  },
+  copy: {
+    patterns: [
+    ],
+    options: {
+    }
+  },
+  framework: 'vue3',
+  compiler: 'webpack5',
+  cache: {
+    enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+  },
+  sass: {
+    data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`
+  },
+  mini: {
+    webpackChain(chain) {
+      // https://github.com/antfu/unplugin-auto-import
+      chain.plugin('unplugin-auto-import').use(AutoImport({
+        imports: [
+          'vue',
+          // https://vuejs.org/guide/extras/reactivity-transform.html#refs-vs-reactive-variables
+          'vue/macros',
+        ],
+        dts: 'types/auto-imports.d.ts',
+        dirs: [
+          'src/composables',
+          'src/stores',
+        ],
+        vueTemplate: true,
+      }))
+      // 添加组件按需引入, 自动引入 `src/components` 目录下的组件
+      // https://github.com/antfu/unplugin-vue-components
+      chain.plugin('unplugin-vue-components').use(Components({
+        dts: 'types/components.d.ts',
+        dirs: ['src/components', 'src/layouts'],
+      }))
+      chain.merge({
+        module: {
+          rule: {
+            mjsScript: {
+              test: /\.mjs$/,
+              include: [/pinia/, /unplugin-vue-components/, /unplugin-auto-import/],
+              use: {
+                babelLoader: {
+                  loader: require.resolve('babel-loader'),
+                },
+              },
+            },
+          },
+        },
+      })
+    },
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {
+          selectorBlackList: ['nut-']
+        }
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024 // 设定转换尺寸上限
+        }
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  },
+  h5: {
+    publicPath: '/',
+    staticDirectory: 'static',
+    esnextModules: ['nutui-taro'],
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {
+        }
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  }
+}
+
+module.exports = function (merge) {
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'))
+  }
+  return merge({}, config, require('./prod'))
+}
---
+++ src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="index p-80">
+    {{ state.msg }}
+    <div class="btn">
+      <nut-button type="primary" @click="handleClick('text', state.msg2, true)">点我</nut-button>
+    </div>
+    <nut-toast :msg="state.msg" v-model:visible="state.show" :type="state.type" :cover="state.cover" />
+  </div>
+</template>
+<script lang="ts" setup>
+const state = ref({
+  msg: '欢迎使用 NutUI3.0 开发小程序',
+  msg2: '你成功了～',
+  type: 'text',
+  show: false,
+  cover: false,
+})
+const handleClick = (type, msg, cover = false) => {
+  state.value.show = true
+  state.value.msg2 = msg
+  state.value.type = type
+  state.value.cover = cover
+}
+</script>
+
+<style lang="scss">
+.index {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
